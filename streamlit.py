5438b9d643dc76ecc4d9a4fd4ee4938134e11f4c
Nazwa Tri Ananda <nzwatriananda@gmail.com>
2025-12-05 06:03:37 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,8 @@ import tempfile
 import os
 import re
 from moviepy.editor import AudioFileClip
-from transformers import pipeline
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from datasets import load_dataset
 
 # -----------------------
 # CONFIG
@@ -51,12 +52,24 @@ def video_to_audio(video_path):
     return tmp_audio.name
 
 def whisper_transcribe(video_path, pipe):
-    audio_path = video_to_audio(video_path)
-    try:
-        result = pipe(audio_path)
-        return result["text"]
-    finally:
-        os.remove(audio_path)
+    # load model and processor
+    processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2")
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v2")
+    model.config.forced_decoder_ids = None
+    
+    # load dummy dataset and read audio files
+    ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+    sample = ds[0]["audio"]
+    input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
+    
+    # generate token ids
+    predicted_ids = model.generate(input_features)
+    # decode token ids to text
+    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=False)
+    ['<|startoftranscript|><|en|><|transcribe|><|notimestamps|> Mr. Quilter is the apostle of the middle classes and we are glad to welcome his gospel.<|endoftext|>']
+    
+    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+    [' Mr. Quilter is the apostle of the middle classes and we are glad to welcome his gospel.']
 
 def mistral_lora_api(prompt):
     """Call fine-tuned Mistral model on HuggingFace Inference API."""
